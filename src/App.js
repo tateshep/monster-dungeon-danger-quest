517fd4a8212c4c7aef8778c00c8bb8ef8e6aefa5
Tate Shepherd <tshepherd@kollecitve.com>
2019-11-08 16:23:29 -0800

    added styling to chatbot. Changed bot image for old man

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import Game from './Game/Game'
 import './App.css';
 
@@ -8,15 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
         </header>
         <body className="App-body">
           <Game />
--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 import GameInfo from './GameInfo/GameInfo';
 import createMonster from './Monsters/MonsterCreate';
 import PlayerInfo from './Player/PlayerInfo';
+import OldMan from '../assets/images/OldMan.png';
+
+const chatbotTheme = {
+    background: '#0D1317',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: '#E5F2C9',
+    headerFontColor: '#000',
+    headerFontSize: '15px',
+    botBubbleColor: '#E5F2C9',
+    botFontColor: '#000',
+    userBubbleColor: '#40434E',
+    userFontColor: '#fff',
+}
 
 class Game extends Component {
     constructor(props) {
@@ -15,51 +29,53 @@ class Game extends Component {
             currentlyFighting: false
         }
     }
-
     render() {
         return (
             <>
                 <GameInfo score={this.state.score} player={this.state.player} />
-                <ChatBot
-                    width={`600px`}
-                    // speechSynthesis={{ enable: true, lang: 'en' }}
-                    steps={
-                        [
-                            {
-                              id: `game-start`,
-                              message: `Welcome adventurer! Thank you for coming to save us from these terrible monsters. What is your name?`,
-                              trigger: `player-create`
-                            },
-                            {
-                              id: `player-create`,
-                              user: true,
-                              validator: (value) => {
-                                if (value.length > 20) {
-                                    return 'Name too long. Keep it to 20 characters or less.';
-                                } else {
-                                    return true;
+                <ThemeProvider theme={chatbotTheme}>
+                    <ChatBot
+                        botAvatar={OldMan}
+                        width={`600px`}
+                        // speechSynthesis={{ enable: true, lang: 'en' }}
+                        steps={
+                            [
+                                {
+                                id: `game-start`,
+                                message: `Welcome adventurer! Thank you for coming to save us from these terrible monsters. What is your name?`,
+                                trigger: `player-create`
+                                },
+                                {
+                                id: `player-create`,
+                                user: true,
+                                validator: (value) => {
+                                    if (value.length > 20) {
+                                        return 'Name too long. Keep it to 20 characters or less.';
+                                    } else {
+                                        return true;
+                                    }
+                                },
+                                trigger: `hello-hero`
+                                },
+                                {
+                                id: `hello-hero`,
+                                message: ({ previousValue }) => {
+                                    this.setState({ player: { name: previousValue }});
+                                    return `Greetings ${previousValue}. We are so happy you have come to help us. The dungeon awaits you, who knows what lurks in the darkness...`
+                                },
+                                trigger: `enter-dungeon`
+                                },
+                                {
+                                    id: `enter-dungeon`,
+                                    component: (
+                                        <div> The Dungeon of Horrors </div>
+                                    ),
+                                    end: true
                                 }
-                            },
-                              trigger: `hello-hero`
-                            },
-                            {
-                              id: `hello-hero`,
-                              message: ({ previousValue }) => {
-                                this.setState({ player: { name: previousValue }});
-                                return `Greetings ${previousValue}. We are so happy you have come to help us. The dungeon awaits you, who knows what lurks in the darkness...`
-                              },
-                              trigger: `enter-dungeon`
-                            },
-                            {
-                                id: `enter-dungeon`,
-                                component: (
-                                    <div> The Dungeon of Horrors </div>
-                                ),
-                                end: true
-                            }
-                          ]
-                    }
-                />
+                            ]
+                        }
+                    />
+                </ThemeProvider>
             </>
         )
     }
